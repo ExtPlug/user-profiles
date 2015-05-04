1070d32831cbc4f088a1be2b31dfa1bdffc7b3b8
René Kooi <rene@kooi.me>
2015-05-04 18:33:23 +0200

    0.0.2

--- build/user-profiles.js
+++ build/user-profiles.js
@@ -111,7 +111,11 @@ define('extplug/user-profiles/ProfileView',['require','exports','module','jquery
         top: '0',
         left: '0',
         width: '100%',
-        height: '100%'
+        height: '100%',
+
+        // hide Points and Subscribe buttons, since
+        // they always show the current user's data
+        '.meta .points': { display: 'none' }
       }
     }
   });
@@ -134,110 +138,20 @@ define('extplug/user-profiles/HistoryPanelView',['require','exports','module','p
 });
 
 
-var _toArray = function (arr) { return Array.isArray(arr) ? arr : Array.from(arr); };
-
-define('extplug/user-profiles/HistoryView',['require','exports','module','jquery','backbone','plug/models/currentMedia','plug/models/HistoryEntry','plug/models/Room','plug/models/Media','plug/util/util','./HistoryPanelView'],function (require, exports, module) {
+define('extplug/user-profiles/HistoryView',['require','exports','module','jquery','backbone','./HistoryPanelView'],function (require, exports, module) {
   var $ = require('jquery');
   var Backbone = require('backbone');
-  var currentMedia = require('plug/models/currentMedia');
-  var HistoryEntry = require('plug/models/HistoryEntry');
-  var Room = require('plug/models/Room');
-  var Media = require('plug/models/Media');
-  var util = require('plug/util/util');
   var HistoryPanelView = require('./HistoryPanelView');
-
-  function toInt(el) {
-    return parseInt(el.find('span').text().trim(), 10);
-  }
-
-  function getHistory(user, cb) {
-    if (user.get('slug')) {
-      next(user.get('slug'));
-    } else if (user.get('level') >= 5) {
-      new UserFindAction(user.id).on('success', function (user) {
-        if (!user.get('slug')) return;
-        next(user.get('slug'));
-      });
-    }
-
-    function next(slug) {
-      $.get('https://plug.dj/@/' + user.get('slug'), parseProfileBody);
-    }
-    function parseProfileBody(body) {
-      // get the actual body, without all the link and script tags
-      var bodyStart = body.indexOf('<body>');
-      var bodyEnd = body.indexOf('</body>', bodyStart + 1);
-      body = body.substring(bodyStart + 6, bodyEnd);
-      // prevent the browser from trying to load all the youtube images
-      // already
-      body = body.replace(/img src=/g, 'img data-src=');
-      var history = [];
-      $('.row', body).each(function () {
-        var el = $(this);
-
-        var _el$find$text$split = el.find('.author').text().split(' - ');
-
-        var _el$find$text$split2 = _toArray(_el$find$text$split);
-
-        var author = _el$find$text$split2[0];
-
-        var title = _el$find$text$split2.slice(1);
-
-        var image = el.find('img[data-src]').data('src');
-        var media = new Media({
-          author: author.trim(),
-          title: title.join(' - ').trim(),
-          image: image
-        });
-        if (image.indexOf('ytimg') !== -1) {
-          media.set({
-            format: 1,
-            // extract video id
-            cid: image.match(/\/([a-zA-Z0-9_\-]{11,})\//)[1]
-          });
-        } else {
-          // soundcloud video, we can't really extract the ID properly
-          media.set({ format: 2 });
-        }
-        var he = new HistoryEntry({
-          user: user,
-          media: media,
-          room: {
-            name: el.find('.name').text().trim()
-          },
-          timestamp: util.convertUnixDateStringToNumberString(el.find('.timestamp').text().trim()),
-          score: {
-            positive: toInt(el.find('.score .positive')),
-            negative: toInt(el.find('.score .negative')),
-            curates: toInt(el.find('.score .grabs')),
-            listeners: toInt(el.find('.score .listeners'))
-          }
-        });
-        history.push(he);
-      });
-      cb(null, history);
-    }
-  }
 
   var HistoryView = Backbone.View.extend({
     id: 'extplug-user-profiles-history',
     className: 'user-content',
-    initialize: function initialize() {
-      var _this = this;
-
-      this.history = new Backbone.Collection();
-      if (this.model) {
-        getHistory(this.model, function (e, history) {
-          _this.history.reset(history);
-        });
-      }
-    },
     render: function render() {
       if (this.model.get('level') < 5) {
         this.$el.append('\n          You can only view the history of users above level 5,\n          but ' + this.model.get('username') + ' is only level\n          ' + this.model.get('level') + '.\n        ');
         return this;
       }
-      this.list = new HistoryPanelView({ collection: this.history });
+      this.list = new HistoryPanelView({ collection: this.model.get('history') });
       this.$el.append(this.list.$el);
       this.list.render();
       return this;
@@ -254,29 +168,32 @@ define('extplug/user-profiles/HistoryView',['require','exports','module','jquery
 });
 
 
-define('extplug/user-profiles/CommunitiesView',['require','exports','module','jquery','backbone','extplug/util/Style','plug/actions/rooms/ListRoomsAction','plug/views/dashboard/list/CellView','plug/models/Room'],function (require, exports, module) {
+define('extplug/user-profiles/CommunitiesView',['require','exports','module','jquery','extplug/util/Style','plug/actions/rooms/ListRoomsAction','plug/views/users/communities/CommunitiesView','plug/views/users/communities/CommunityGridView','plug/models/Room','plug/util/window','plug/core/Events','plug/events/ShowDialogEvent','plug/views/dialogs/RoomCreateDialog'],function (require, exports, module) {
   var $ = require('jquery');
 
-  var _require = require('backbone');
-
-  var View = _require.View;
-
   var Style = require('extplug/util/Style');
+
   var ListRoomsAction = require('plug/actions/rooms/ListRoomsAction');
-  var CommunityView = require('plug/views/dashboard/list/CellView');
+  // used for drawing rooms
+  var Base = require('plug/views/users/communities/CommunitiesView');
+  var CommunityGridView = require('plug/views/users/communities/CommunityGridView');
   var Room = require('plug/models/Room');
 
-  var CommunitiesView = View.extend({
+  var _require = require('plug/util/window');
+
+  var getSize = _require.getSize;
+
+  // used for the Create Room link
+  var Events = require('plug/core/Events');
+  var ShowDialogEvent = require('plug/events/ShowDialogEvent');
+  var RoomCreateDialog = require('plug/views/dialogs/RoomCreateDialog');
+
+  var CommunitiesView = Base.extend({
+    id: null,
     className: 'user-content communities',
     render: function render() {
-      this.$container = $('<div />').addClass('container');
-      this.$rooms = $('<div />').addClass('grid');
-      this.$el.append(this.$container);
-      this.$container.append(this.$rooms);
-      this.$container.jScrollPane();
-      this.scrollPane = this.$container.data('jsp');
-      this.cells = [];
       this.getCommunities(this.model.get('username'));
+      this.allowResize = true;
       return this;
     },
     getCommunities: function getCommunities(host) {
@@ -290,29 +207,35 @@ define('extplug/user-profiles/CommunitiesView',['require','exports','module','jq
         throw e;
       });
     },
-    onResize: function onResize(size) {},
     setRooms: function setRooms(rooms) {
       var _this2 = this;
 
-      this.cells = rooms.map(function (room) {
-        return new CommunityView({ model: new Room(room) });
+      if (!this.$el) {
+        return;
+      }this.$top = $('<div />').addClass('top');
+      this.$message = $('<div />').addClass('message');
+      this.$box = $('<div />').addClass('box');
+      this.$el.empty().append(this.$top.append(this.$message)).append(this.$box);
+      this.$box.jScrollPane();
+      this.scrollPane = this.$box.data('jsp');
+
+      this.grid = new CommunityGridView();
+      this.$box.append(this.grid.$el);
+      if (rooms.length === 0) {
+        this.grid.clear();
+        this.$message.text('This user has not created any communities.');
+      } else {
+        this.$message.text('These are ' + this.model.get('username') + '\'s communities. ').append('Click ', $('<a />').attr('href', '#').text('here').on('click', function () {
+          Events.dispatch(new ShowDialogEvent(ShowDialogEvent.SHOW, new RoomCreateDialog()));
+        }), ' to make your own!');
+        rooms.map(function (room) {
+          return _this2.grid.draw(new Room(room));
+        });
+      }
+      this.grid.onUpdate();
+      _.defer(function () {
+        return _this2.onResize(getSize());
       });
-      this.cells.forEach(function (cell) {
-        cell.render();
-        _this2.$rooms.append(cell.$el);
-      });
-      this.scrollPane.reinitialise();
-    },
-    remove: function remove() {
-      this.cells.forEach(function (cell) {
-        return cell.destroy();
-      });
-      this.cells = [];
-      this.scrollPane && this.scrollPane.destroy();
-      this.scrollPane = null;
-      this.$container.remove();
-      this.$container = null;
-      this._super();
     }
   });
 
@@ -402,19 +325,129 @@ define('extplug/user-profiles/UserView',['require','exports','module','extplug/u
 });
 
 
-define('extplug/user-profiles/main',['require','exports','module','jquery','meld','extplug/Module','plug/actions/users/UserFindAction','plug/models/User','plug/views/users/userRolloverView','./UserView'],function (require, exports, module) {
+var _toArray = function (arr) { return Array.isArray(arr) ? arr : Array.from(arr); };
+
+define('extplug/user-profiles/profile-info',['require','exports','module','jquery','backbone','plug/actions/users/UserFindAction','plug/util/util','plug/models/Media','plug/models/HistoryEntry','extplug/util/request'],function (require, exports, module) {
+
+  var $ = require('jquery');
+
+  var _require = require('backbone');
+
+  var Collection = _require.Collection;
+
+  var UserFindAction = require('plug/actions/users/UserFindAction');
+  var util = require('plug/util/util');
+  var Media = require('plug/models/Media');
+  var HistoryEntry = require('plug/models/HistoryEntry');
+  var request = require('extplug/util/request');
+
+  // ensures that the user object has a "slug" property
+  function fetchSlug(user) {
+    return $.Deferred(function (d) {
+      if (user.get('slug')) {
+        return d.resolve(user);
+      }
+      if (user.get('level') < 5) {
+        return d.reject(new Error('User is below level 5 and ' + 'doesn\'t have a profile page.'));
+      }
+      new UserFindAction(user.get('id')).on('success', function (data) {
+        user.set(data);
+        d.resolve(user);
+      }).on('error', d.reject);
+    });
+  }
+
+  // gets the HTML contents of a user profile page
+  function fetchPage(user) {
+    return request('https://plug.dj/@/' + user.get('slug'));
+  }
+
+  // parses a user profile page into a "blurb" and a DJ history collection
+  var toInt = function toInt(el) {
+    return parseInt(el.find('span').text().trim(), 10);
+  };
+  function parse(user, body) {
+    // get the actual body, without all the link and script tags
+    var bodyStart = body.indexOf('<body>');
+    var bodyEnd = body.indexOf('</body>', bodyStart + 1);
+    body = body.substring(bodyStart + 6, bodyEnd);
+    // prevent the browser from trying to load all the thumbnail images
+    // already
+    body = body.replace(/img src=/g, 'img data-src=');
+    var blurb = $('.blurb .box', body).text().trim();
+    var history = parseHistory(user, body);
+    return user.set({ blurb: blurb, history: history });
+  }
+  function parseHistory(user, body) {
+    var history = $('.row', body).map(function () {
+      var el = $(this);
+
+      var _el$find$text$split = el.find('.author').text().split(' - ');
+
+      var _el$find$text$split2 = _toArray(_el$find$text$split);
+
+      var author = _el$find$text$split2[0];
+
+      var title = _el$find$text$split2.slice(1);
+
+      var image = el.find('img[data-src]').data('src');
+      var media = new Media({
+        author: author.trim(),
+        title: title.join(' - ').trim(),
+        image: image
+      });
+      if (image.indexOf('ytimg') !== -1) {
+        media.set({
+          format: 1,
+          // extract video id
+          cid: (image.match(/\/([a-zA-Z0-9_\-]{11,})\//) || [''])[1]
+        });
+      } else {
+        // soundcloud video, we can't really extract the ID properly
+        media.set({ format: 2 });
+      }
+      return new HistoryEntry({
+        user: user,
+        media: media,
+        room: { name: el.find('.name').text().trim() },
+        timestamp: util.convertUnixDateStringToNumberString(el.find('.timestamp').text().trim()),
+        score: {
+          positive: toInt(el.find('.score .positive')),
+          negative: toInt(el.find('.score .negative')),
+          curates: toInt(el.find('.score .grabs')),
+          listeners: toInt(el.find('.score .listeners'))
+        }
+      });
+    });
+    return new Collection(history.toArray());
+  }
+
+  // gets full user profile info, including slug, blurb and play history
+  function getProfileInfo(user) {
+    return fetchSlug(user).then(fetchPage).then(function (body) {
+      return parse(user, body);
+    });
+  }
+
+  module.exports = getProfileInfo;
+});
+
+
+define('extplug/user-profiles/main',['require','exports','module','jquery','meld','extplug/Plugin','plug/actions/users/UserFindAction','plug/models/User','plug/collections/users','plug/views/users/userRolloverView','./UserView','./profile-info'],function (require, exports, module) {
 
   var $ = require('jquery');
   var meld = require('meld');
 
-  var Module = require('extplug/Module');
+  var Plugin = require('extplug/Plugin');
   var UserFindAction = require('plug/actions/users/UserFindAction');
   var User = require('plug/models/User');
+  var users = require('plug/collections/users');
   var rolloverView = require('plug/views/users/userRolloverView');
 
   var UserView = require('./UserView');
+  var getProfileInfo = require('./profile-info');
 
-  module.exports = Module.extend({
+  module.exports = Plugin.extend({
     name: 'User Profiles',
 
     enable: function enable() {
@@ -445,17 +478,24 @@ define('extplug/user-profiles/main',['require','exports','module','jquery','meld
     showProfile: function showProfile(id) {
       var _this2 = this;
 
-      new UserFindAction(id).on('success', function (user) {
-        if (_this2.userView) {
-          _this2.userView.model = new User(user);
-          _this2.userView.render();
-        } else {
-          _this2.userView = new UserView({ model: new User(user) });
-          _this2.userView.render();
-          _this2.userView.$el.appendTo('body');
-        }
-        _this2.userView.show('profile');
-      });
+      var show = function show(user) {
+        getProfileInfo(user).then(function (user) {
+          if (_this2.userView) {
+            _this2.userView.model = user.clone();
+            _this2.userView.render();
+          } else {
+            _this2.userView = new UserView({ model: user.clone() });
+            _this2.userView.render();
+            _this2.userView.$el.appendTo('body');
+          }
+          _this2.userView.show('profile');
+        });
+      };
+      if (users.get(id)) {
+        show(users.get(id));
+      } else {
+        new UserFindAction(id).on('success', show);
+      }
     }
   });
 });
