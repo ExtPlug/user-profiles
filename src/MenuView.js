a29c35cf2712ddd8d366ebdec367745949abebd6
René Kooi <renee@kooi.me>
2017-01-19 11:19:58 +0100

    ES Modulesify.

--- src/MenuView.js
+++ src/MenuView.js
@@ -1,51 +1,46 @@
-define(function (require, exports, module) {
-  const $ = require('jquery');
-  const _ = require('underscore');
-  const Events = require('plug/core/Events');
-  const AlertEvent = require('plug/events/AlertEvent');
-//  var currentUser = require('f3ecc/d813f/a062a');
-//  var BefriendAction = require('f3ecc/a67a5/c3ae3/e62c4');
-//  var UnfriendAction = require('f3ecc/a67a5/c3ae3/ab103');
-  const AvatarManifest = require('plug/util/AvatarManifest');
-  const Lang = require('lang/Lang');
-  const UserMenuView = require('plug/views/users/menu/UserMenuView');
-  const Style = require('extplug/util/Style');
-  var ProfileMenuView = UserMenuView.extend({
-    id: 'extplug-user-profiles-menu',
-    initialize() {
-      this._super();
-      this.onPress = this.onPress.bind(this);
-      this.onClick = this.onClick.bind(this);
-      this.onAvatarChange = this.onAvatarChange.bind(this);
-    },
-    render() {
-      this.$el.html(`
-        <div class="avatar"></div>
-        <div class="item profile" data-value="profile">
-          <i class="icon icon-user-grey"></i>
-          <span class="label">${Lang.userFriends.profile}</span>
-        </div>
-        <div class="item played" data-value="played">
-          <i class="icon icon-history-grey"></i>
-          <span class="label">${Lang.userMenu.played}</span>
-        </div>
-      `);
-      this.$avatar = this.$('.avatar')
-        .on('mousedown', this.onPress);
-      this.$('.item').on('click', this.onClick);
-      this.onAvatarChange();
-      return this;
-    },
-    onAvatarChange() {
-      this.$avatar.empty();
-      this.$img.attr('src', AvatarManifest.getAvatarUrl(this.model.get('avatarID'), 'b'));
-      this.$avatar.append(this.$img);
-      this.aviW = 220;
-      this.aviO = 0;
-      this.model.get('avatarID').indexOf('dragon') > -1 && (this.aviO = -8, this.aviW = 440);
-    }
-  });
+import $ from 'jquery';
+import _ from 'underscore';
+import Events from 'plug/core/Events';
+import AlertEvent from 'plug/events/AlertEvent';
+import AvatarManifest from 'plug/util/AvatarManifest';
+import Lang from 'lang/Lang';
+import UserMenuView from 'plug/views/users/menu/UserMenuView';
+import Style from 'extplug/util/Style';
 
-  return ProfileMenuView;
-
+const ProfileMenuView = UserMenuView.extend({
+  id: 'extplug-user-profiles-menu',
+  initialize() {
+    this._super();
+    this.onPress = this.onPress.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onAvatarChange = this.onAvatarChange.bind(this);
+  },
+  render() {
+    this.$el.html(`
+      <div class="avatar"></div>
+      <div class="item profile" data-value="profile">
+        <i class="icon icon-user-grey"></i>
+        <span class="label">${Lang.userFriends.profile}</span>
+      </div>
+      <div class="item played" data-value="played">
+        <i class="icon icon-history-grey"></i>
+        <span class="label">${Lang.userMenu.played}</span>
+      </div>
+    `);
+    this.$avatar = this.$('.avatar')
+      .on('mousedown', this.onPress);
+    this.$('.item').on('click', this.onClick);
+    this.onAvatarChange();
+    return this;
+  },
+  onAvatarChange() {
+    this.$avatar.empty();
+    this.$img.attr('src', AvatarManifest.getAvatarUrl(this.model.get('avatarID'), 'b'));
+    this.$avatar.append(this.$img);
+    this.aviW = 220;
+    this.aviO = 0;
+    this.model.get('avatarID').indexOf('dragon') > -1 && (this.aviO = -8, this.aviW = 440);
+  }
 });
+
+export default ProfileMenuView;
--- src/ProfileView.js
+++ src/ProfileView.js
@@ -1,33 +1,32 @@
-define(function (require, exports, module) {
-  const $ = require('jquery');
-  const { View } = require('backbone');
-  const MetaView = require('./MetaView');
+import $ from 'jquery';
+import { View } from 'backbone';
+import MetaView from './MetaView';
 
-  const ProfileView = View.extend({
-    className: 'user-content profile',
-    render: function () {
-      this.meta = new MetaView({ model: this.model });
-      this.$container = $('<div />').addClass('container');
-      this.$container.append(this.meta.$el);
-      this.$el.append(this.$container);
-      this.meta.render();
-      this.$container.jScrollPane();
-      this.scrollPane = this.$container.data('jsp');
-      return this;
-    },
-    onResize: function (e) {
-    },
-    remove: function () {
-      this.scrollPane && this.scrollPane.destroy();
-      this.meta.destroy();
-      this.$container.remove();
-      this.scrollPane = null;
-      this.meta = null;
-      this.$container = null;
-      this._super();
-    }
-  });
+const ProfileView = View.extend({
+  className: 'user-content profile',
 
-  module.exports = ProfileView;
+  render() {
+    this.meta = new MetaView({ model: this.model });
+    this.$container = $('<div />').addClass('container');
+    this.$container.append(this.meta.$el);
+    this.$el.append(this.$container);
+    this.meta.render();
+    this.$container.jScrollPane();
+    this.scrollPane = this.$container.data('jsp');
+    return this;
+  },
 
+  onResize(e) {},
+
+  remove() {
+    this.scrollPane && this.scrollPane.destroy();
+    this.meta.destroy();
+    this.$container.remove();
+    this.scrollPane = null;
+    this.meta = null;
+    this.$container = null;
+    this._super();
+  }
 });
+
+export default ProfileView;
